a2d5030cb954fb12ebce4920a1eb29d23209fb0e
Janosch Machowinski <janosch.machowinski@dfki.de>
2015-07-21 09:30:49 +0200

    Initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <state_machine/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    state_machine::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
